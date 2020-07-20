78f33df81557b69fedcfefd5c59750c2ebc015b4
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-07-20 10:24:11 +0700

    Create Authentication routing and link to view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+    	return view('auth/login');
+    }
+
+    public function postLogin(Request $request)
+    {
+
+    }
+
+    public function register()
+    {
+    	return view('auth/register');
+    }
+
+    public function postRegister(Request $request)
+    {
+
+    }
+
+    public function logout()
+    {
+    	Auth::logout();
+    	return redirect('/login')->with('auth', 'logout');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
+
+Route::get('/login', 'AuthController@login');
+Route::post('/login', 'AuthController@postLogin');
+Route::get('/register', 'AuthController@register');
+Route::post('/register', 'AuthController@postRegister');
+
+Route::group(['middleware' => 'auth'], function() {
+	Route::get('/logout', 'AuthController@logout');
+});
