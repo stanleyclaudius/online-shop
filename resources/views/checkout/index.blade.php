3be25ed528cb720d03bf68a5a7dec37457bf471c
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-08-01 23:37:01 +0700

    Change checkout/index page design

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -5,11 +5,12 @@
 	<title>Dev Store | Checkout</title>
 	<link rel="stylesheet" href="{{ asset('css') }}/main.css">
 </head>
-<body>
+<body class="bg-gray-900">
 
-	<div class="shadow-xl py-5 mb-10">
-		<div class="container mx-auto">
-			<p class="text-2xl font-semibold">Dev Store | Checkout</p>
+	<div class="border-b border-gray-700 py-5 mb-10">
+		<div class="container mx-auto flex items-center justify-between">
+			<a href="/" class="text-2xl text-white font-semibold">Dev Store</a>
+			<p class="text-2xl text-white font-semibold">Checkout</p>
 		</div>
 	</div>
 
@@ -22,7 +23,7 @@
 						@if($errors->has('name'))
 							<small class="text-red-500">{{ $errors->first('name') }}</small>
 						@else
-							<label for="name" class="text-lg">Name</label>
+							<label for="name" class="text-lg text-white">Name</label>
 						@endif
 						<input type="text" name="name" id="name" class="rounded border mt-3 border-gray-500 w-full h-10 focus:shadow-outline focus:outline-none px-2" value="{{ auth()->user()->name }}">
 					</div>
@@ -30,7 +31,7 @@
 						@if($errors->has('phone'))
 							<small class="text-red-500">{{ $errors->first('phone') }}</small>
 						@else
-							<label for="phone" class="text-lg">Phone</label>
+							<label for="phone" class="text-lg text-white">Phone</label>
 						@endif
 						<input type="number" name="phone" id="phone" class="rounded border mt-3 border-gray-500 w-full h-10 focus:shadow-outline focus:outline-none px-2" value="{{ auth()->user()->phone }}">
 					</div>
@@ -40,7 +41,7 @@
 						@if($errors->has('country'))
 							<small class="text-red-500">{{ $errors->first('country') }}</small>
 						@else
-							<label for="country" class="text-lg">Country</label>
+							<label for="country" class="text-lg text-white">Country</label>
 						@endif
 						<input type="text" name="country" id="country" class="rounded border mt-3 border-gray-500 w-full h-10 focus:shadow-outline focus:outline-none px-2" value="{{ auth()->user()->country }}">
 					</div>
@@ -48,7 +49,7 @@
 						@if($errors->has('province'))
 							<small class="text-red-500">{{ $errors->first('province') }}</small>
 						@else
-							<label for="province" class="text-lg">Province</label>
+							<label for="province" class="text-lg text-white">Province</label>
 						@endif
 						<input type="text" name="province" id="province" class="rounded border mt-3 border-gray-500 w-full h-10 focus:shadow-outline focus:outline-none px-2" value="{{ auth()->user()->province }}">
 					</div>
@@ -58,7 +59,7 @@
 						@if($errors->has('city'))
 							<small class="text-red-500">{{ $errors->first('city') }}</small>
 						@else
-							<label for="city" class="text-lg">City</label>
+							<label for="city" class="text-lg text-white">City</label>
 						@endif
 						<input type="text" name="city" id="city" class="rounded border mt-3 border-gray-500 w-full h-10 focus:shadow-outline focus:outline-none px-2" value="{{ auth()->user()->city }}">
 					</div>
@@ -68,23 +69,23 @@
 						@if($errors->has('address'))
 							<small class="text-red-500">{{ $errors->first('address') }}</small>
 						@else
-							<label for="address" class="text-lg">Address</label>
+							<label for="address" class="text-lg text-white">Address</label>
 						@endif
 						<textarea name="address" rows="4" id="address" class="rounded border mt-3 border-gray-500 w-full focus:shadow-outline focus:outline-none px-2">{{ auth()->user()->address }}</textarea>
 					</div>
 				</div>
 				<input type="hidden" value="{{ $totalPrice }}" class="totalPriceHidden" name="initialtotalprice"> <!-- Constant value initial price after checkout -->
 				<input type="hidden" class="discountValueHidden" name="finalResultHidden"> <!-- Price after discount -->
-				<button type="submit" class="bg-gray-900 transition duration-150 ease-in-out hover:bg-gray-800 rounded px-3 py-2 text-white mt-4">Proceed</button>
+				<button type="submit" class="bg-gray-800 transition duration-150 ease-in-out hover:bg-gray-700 rounded px-3 py-2 text-white mt-4">Proceed</button>
 			</form>
 		</div>
 		<div>
 			@if($items->count() == 1)
-				<p class="font-semibold text-lg uppercase">checkout item</p>
+				<p class="font-semibold text-white text-lg uppercase">checkout item</p>
 			@else
-				<p class="font-semibold text-lg uppercase">checkout items</p>
+				<p class="font-semibold text-white text-lg uppercase">checkout items</p>
 			@endif
-			<div class="border rounded border-gray-500 w-full mt-3">
+			<div class="border rounded border-gray-700 w-full mt-3">
 				<ul>
 					@foreach($items as $item)
 						@php
@@ -94,12 +95,12 @@
 							<li class="border-b border-gray-500 p-4 flex items-center">
 								<img src="{{ asset('img') }}/products/{{ $item->product->product_image }}" alt="" width="80">
 								<div class="ml-4">
-									<p class="text-lg">{{ $item->product->product_name }}</p>
-									<p>QTY : {{ $item->qty }}</p>
+									<p class="text-lg text-white">{{ $item->product->product_name }}</p>
+									<p class="text-white">QTY : {{ $item->qty }}</p>
 									@if($item->total == null)
-										<p class="font-semibold mt-2">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
+										<p class="font-semibold mt-2 text-white">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
 									@else
-										<p class="font-semibold mt-2">{{ 'Rp.' . $price }}</p>
+										<p class="font-semibold mt-2 text-white">{{ 'Rp.' . $price }}</p>
 									@endif
 								</div>
 							</li>
@@ -108,12 +109,12 @@
 							<li class="p-4 flex items-center">
 								<img src="{{ asset('img') }}/products/{{ $item->product->product_image }}" alt="" width="80">
 								<div class="ml-4">
-									<p class="text-lg">{{ $item->product->product_name }}</p>
-									<p>QTY : {{ $item->qty }}</p>
+									<p class="text-lg text-white">{{ $item->product->product_name }}</p>
+									<p class="text-white">QTY : {{ $item->qty }}</p>
 									@if($item->total == null)
-										<p class="font-semibold mt-2">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
+										<p class="font-semibold mt-2 text-white">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
 									@else
-										<p class="font-semibold mt-2">{{ 'Rp.' . $price }}</p>
+										<p class="font-semibold mt-2 text-white">{{ 'Rp.' . $price }}</p>
 									@endif
 								</div>
 							</li>
@@ -121,28 +122,28 @@
 					@endforeach
 				</ul>
 			</div>
-			<p class="text-lg font-semibold uppercase mt-8">discount voucher</p>
-			<div class="border rounded border-gray-500 w-full mt-3">
+			<p class="text-lg font-semibold uppercase mt-5 text-white">discount voucher</p>
+			<div class="border rounded border-gray-700 w-full mt-3">
 				<div class="grid grid-cols-3 p-4 gap-5">
 					<div class="col-span-2">
 						<input type="text" class="uppercase h-10 rounded px-2 border border-gray-500 w-full discount-code">
 					</div>
-					<button class="discount-check bg-gray-900 transition duration-150 ease-in-out hover:bg-gray-800 text-white rounded px-2 py-2">Check</button>
+					<button class="discount-check bg-gray-800 transition duration-150 ease-in-out hover:bg-gray-700 text-white rounded px-2 py-2">Check</button>
 				</div>
 			</div>
-			<div class="border border-gray-500 rounded mt-8">
+			<div class="border border-gray-700 rounded mt-6">
 				<ul>
-					<li class="p-4 border-b border-gray-500 flex items-center justify-between">
-						<p class="font-semibold uppercase text-lg">subtotal</p>
-						<p class="text-lg">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
+					<li class="p-4 border-b border-gray-700 flex items-center justify-between">
+						<p class="font-semibold uppercase text-lg text-white">subtotal</p>
+						<p class="text-lg text-white">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
 					</li>
-					<li class="p-4 border-b border-gray-500 flex items-center justify-between">
-						<p class="font-semibold uppercase text-lg">DISCOUNT</p>
-						<p class="text-lg discount-initial">0</p>
+					<li class="p-4 border-b border-gray-700 flex items-center justify-between">
+						<p class="font-semibold uppercase text-lg text-white">DISCOUNT</p>
+						<p class="text-lg discount-initial text-white">Rp.0,00</p>
 					</li>
 					<li class="p-4 flex items-center justify-between">
-						<p class="font-semibold uppercase text-lg">TOTAL</p>
-						<p class="text-lg discount-initial totalAfterDiscount">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
+						<p class="font-semibold uppercase text-lg text-white">TOTAL</p>
+						<p class="text-lg discount-initial totalAfterDiscount text-white">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
 					</li>
 				</ul>
 			</div>
@@ -168,7 +169,7 @@
 						let discountValue = data.value;
 						let finalPrice = totalPrice - (discountValue/100*totalPrice);
 						$('.discountValueHidden').val(finalPrice);
-						$('.discount-initial').text('Rp. ' + (discountValue/100*totalPrice) + ',00');
+						$('.discount-initial').text('Rp.' + (discountValue/100*totalPrice) + ',00');
 						$('.totalAfterDiscount').text('Rp.' + finalPrice + ',00');
 						swal.fire({
 							title: 'Success',
@@ -177,7 +178,7 @@
 						});
 					} else {
 						$('.discountValueHidden').val($('.totalPriceHidden').val());
-						$('.discount-initial').text('0');
+						$('.discount-initial').text('Rp.0,00');
 						$('.totalAfterDiscount').text('Rp.' + $('.totalPriceHidden').val() + ',00');
 						swal.fire({
 							title: 'Failed',
