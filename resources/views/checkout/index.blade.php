4eeff74f47b7c9a9371d89358c3afa1f20ac9bd0
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-08-22 14:51:25 +0700

    Make checkout page responsive

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -7,15 +7,85 @@
 </head>
 <body class="bg-gray-900">
 
-	<div class="border-b border-gray-700 py-5 mb-10">
+	<div class="border-b border-gray-700 py-5 mb-10 px-6 sm:px-6 md:px-6 lg:px-0">
 		<div class="container mx-auto flex items-center justify-between">
 			<a href="/" class="text-2xl text-white font-semibold">Dev Store</a>
 			<p class="text-2xl text-white font-semibold">Checkout</p>
 		</div>
 	</div>
 
-	<div class="container mx-auto grid grid-cols-3 gap-20">
-		<div class="col-span-2">
+	<div class="container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-12 sm:gap-12 md:gap-12 lg:gap-20">
+		<div class="px-6 sm:px-6 md:px-6 lg:px-0 block sm:block md:block lg:hidden">
+			@if($items->count() == 1)
+				<p class="font-semibold text-white text-lg uppercase">checkout item</p>
+			@else
+				<p class="font-semibold text-white text-lg uppercase">checkout items</p>
+			@endif
+			<div class="border rounded border-gray-700 w-full mt-3">
+				<ul>
+					@foreach($items as $item)
+						@php
+							$price = number_format($item->total, 2, ',', '.');
+						@endphp
+						@if(!$loop->last)
+							<li class="border-b border-gray-500 p-4 flex items-center">
+								<img src="{{ asset('img') }}/products/{{ $item->product->product_image }}" alt="" width="80">
+								<div class="ml-4">
+									<p class="text-lg text-white">{{ $item->product->product_name }}</p>
+									<p class="text-white">QTY : {{ $item->qty }}</p>
+									@if($item->total == null)
+										<p class="font-semibold mt-2 text-white">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
+									@else
+										<p class="font-semibold mt-2 text-white">{{ 'Rp.' . $price }}</p>
+									@endif
+								</div>
+							</li>
+						@endif
+						@if($loop->last)
+							<li class="p-4 flex items-center">
+								<img src="{{ asset('img') }}/products/{{ $item->product->product_image }}" alt="" width="80">
+								<div class="ml-4">
+									<p class="text-lg text-white">{{ $item->product->product_name }}</p>
+									<p class="text-white">QTY : {{ $item->qty }}</p>
+									@if($item->total == null)
+										<p class="font-semibold mt-2 text-white">@php echo "Rp." . number_format($item->product->product_price, 2, ',', '.') @endphp</p>
+									@else
+										<p class="font-semibold mt-2 text-white">{{ 'Rp.' . $price }}</p>
+									@endif
+								</div>
+							</li>
+						@endif
+					@endforeach
+				</ul>
+			</div>
+			<p class="text-lg font-semibold uppercase mt-5 text-white">discount voucher</p>
+			<div class="border rounded border-gray-700 w-full mt-3">
+				<div class="grid grid-cols-3 p-4 gap-5">
+					<div class="col-span-2">
+						<input type="text" class="uppercase h-10 rounded px-2 border border-gray-500 w-full discount-code">
+					</div>
+					<button class="discount-check bg-gray-800 transition duration-150 ease-in-out hover:bg-gray-700 text-white rounded px-2 py-2">Check</button>
+				</div>
+			</div>
+			<div class="border border-gray-700 rounded mt-6">
+				<ul>
+					<li class="p-4 border-b border-gray-700 flex items-center justify-between">
+						<p class="font-semibold uppercase text-lg text-white">subtotal</p>
+						<p class="text-lg text-white">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
+					</li>
+					<li class="p-4 border-b border-gray-700 flex items-center justify-between">
+						<p class="font-semibold uppercase text-lg text-white">DISCOUNT</p>
+						<p class="text-lg discount-initial text-white">Rp.0,00</p>
+					</li>
+					<li class="p-4 flex items-center justify-between">
+						<p class="font-semibold uppercase text-lg text-white">TOTAL</p>
+						<p class="text-lg discount-initial totalAfterDiscount text-white">@php echo "Rp." . number_format($totalPrice, 2, ',', '.') @endphp</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="col-span-0 sm:col-span-0 md:col-span-0 lg:col-span-2 px-6 sm:px-6 md:px-6 lg:px-0 mb-10 sm:mb-10 md:mb-10 lg:mb-0">
 			<form action="/checkout" method="post">
 				@csrf
 				<div class="grid grid-cols-2 gap-5">
@@ -79,7 +149,8 @@
 				<button type="submit" class="bg-gray-800 transition duration-150 ease-in-out hover:bg-gray-700 rounded px-3 py-2 text-white mt-4">Proceed</button>
 			</form>
 		</div>
-		<div>
+		<!-- DESKTOP VERSION -->
+		<div class="px-6 sm:px-6 md:px-6 lg:px-0 mb-10 hidden sm:hidden md:hidden lg:block">
 			@if($items->count() == 1)
 				<p class="font-semibold text-white text-lg uppercase">checkout item</p>
 			@else
