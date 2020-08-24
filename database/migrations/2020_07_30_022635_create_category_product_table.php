2a0ca81a0544c584fb7025e2544a045205cdad7f
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-08-24 15:44:28 +0700

    Fix error in Product Category migration

--- database/migrations/2020_07_30_022635_create_category_product_table.php
+++ database/migrations/2020_07_30_022635_create_category_product_table.php
@@ -14,15 +14,18 @@ class CreateCategoryProductTable extends Migration
     public function up()
     {
         Schema::create('category_product', function (Blueprint $table) {
+            // $table->id();
+
+            // $table->integer('product_id')->unsigned()->nullable();
+            // $table->foreign('product_id')->references('id')->on('products')
+            //       ->onDelete('cascade');
+
+            // $table->integer('category_id')->unsigned()->nullable();
+            // $table->foreign('category_id')->references('id')->on('categories')
+            //   ->onDelete('cascade');
             $table->id();
-
-            $table->integer('product_id')->unsigned()->nullable();
-            $table->foreign('product_id')->references('id')->on('products')
-                  ->onDelete('cascade');
-
-            $table->integer('category_id')->unsigned()->nullable();
-            $table->foreign('category_id')->references('id')->on('categories')
-              ->onDelete('cascade');
+            $table->integer('product_id');
+            $table->integer('category_id');
         });
     }
 
