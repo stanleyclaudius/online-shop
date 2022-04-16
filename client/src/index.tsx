14a862fbdd4cdf138eca437cf4bba29cdec5fbfa
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-04-16 08:29:15 +0700

    Re-deploy app with ReactJS version 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,13 @@
 import { Provider } from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import store from './redux/store'
 import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
